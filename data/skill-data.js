a38639750aa82f4955d50976a478154b181e20d6
Pin Chen <pamyc1876@gmail.com>
2023-01-04 23:57:13 -0500

    Define dev-skill schema, compile it into model, use it to read data. Delete no-needed data directory. Finish lab part 2

--- data/skill-data.js
+++
@@ -1,9 +0,0 @@
-const skills = [
-    {text: 'C++', learned: true, _id: 125223},
-    {text: 'Javascript', learned: true, _id: 127904},
-    {text: 'MongooseDB', learned: false, _id: 139608},
-  ]
-  
-  export { 
-      skills
-  }
