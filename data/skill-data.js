b3fab1d9bfddb5cac8d69f3060cf35008aa52594
Pin Chen <pamyc1876@gmail.com>
2023-01-04 22:53:36 -0500

    set up skills data

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+    {text: 'C++', done: true, _id: 125223},
+    {text: 'Javascript', done: true, _id: 127904},
+    {text: 'MongooseDB', done: false, _id: 139608},
+  ]
+  
+  export { 
+      skills
+  }
