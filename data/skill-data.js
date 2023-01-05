0c210376a7e99f41bdf8c3c5c3f199d613f45986
Pin Chen <pamyc1876@gmail.com>
2023-01-04 23:10:56 -0500

    set up dev-skill router, add link to dev-skill list

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,7 +1,7 @@
 const skills = [
-    {text: 'C++', done: true, _id: 125223},
-    {text: 'Javascript', done: true, _id: 127904},
-    {text: 'MongooseDB', done: false, _id: 139608},
+    {text: 'C++', learned: true, _id: 125223},
+    {text: 'Javascript', learned: true, _id: 127904},
+    {text: 'MongooseDB', learned: false, _id: 139608},
   ]
   
   export { 
